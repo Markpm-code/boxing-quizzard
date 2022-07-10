351783d936c953f91c38d8249ba509eae799a1c2
Markpm-code <marlonseglos@yahoo.com>
2022-07-10 16:37:25 +0000

    removed previous js code and and Add update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
-const startButton = document.getElementById('start-btn')
-let welcomeDivElement = document.getElementById('intro')
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
-startButton.addEventListener('click', startGame)
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "Let's box to start the game!") {
+                alert("You clicked Let's box to start the game!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
 
-function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    
-}
 
-function  selectQuestion() {
 
-}
